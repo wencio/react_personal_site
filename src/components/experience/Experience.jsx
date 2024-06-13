3dad4a5273787dd9d2061de638677ba448b1548e
wencio daniel martinez <97320103+wencio@users.noreply.github.com>
2024-06-12 20:24:19 -0500

    Update Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -45,7 +45,7 @@ const Experience = () => {
             <article className = "experience__details" >
               <BsFileEarmarkCheckFill className="experience__details-icons" />
               <div>
-              <h4>TensorFlow</h4>
+              <h4>Keras</h4>
               <small className = "text-light">Basic</small>
 
               </div>
@@ -114,7 +114,7 @@ const Experience = () => {
             <article className = "experience__details" >
               <BsFileEarmarkCheckFill className="experience__details-icons" />
               <div>
-              <h4>Keras</h4>
+              <h4>TensorFlow</h4>
               <small className = "text-light">Intermediate</small>
                 </div>
               
